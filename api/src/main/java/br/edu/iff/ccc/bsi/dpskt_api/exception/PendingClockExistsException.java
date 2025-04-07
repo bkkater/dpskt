0bee75d8c021efc1b909ffee0f9f9f51c2b9576f
Bianca Medeiros <biancamedeiros.bs10@gmail.com>
2025-04-07 18:03:48 -0300

    feat: clock exceptions

---
+++ api/src/main/java/br/edu/iff/ccc/bsi/dpskt_api/exception/PendingClockExistsException.java
@@ -0,0 +1,8 @@
+package br.edu.iff.ccc.bsi.dpskt_api.exception;
+
+public class PendingClockExistsException extends RuntimeException {
+
+  public PendingClockExistsException(String message) {
+    super(message);
+  }
+}

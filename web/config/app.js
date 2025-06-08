06e9a7e13ae6fc6ef31055b3c89426e7043c12ea
Bianca Medeiros <biancamedeiros.bs10@gmail.com>
2025-06-08 20:46:07 -0300

    refact: removes js frontend project

--- web/config/app.js
+++
@@ -1,6 +0,0 @@
-export const API_DISCORD_URL = "https://discord.com/api";
-
-export const API =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080/api"
-    : "https://dpskt-api.onrender.com/api";
